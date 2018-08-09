24219dfd82ff1533517ce699e07c433a7afa6aeb
Lance Hunter <robert.l.hunter@oracle.com>
2018-08-09 13:52:15 -0400

    Got basic chatbot connection happening.

--- ChatbotTest/AppDelegate.swift
+++ ChatbotTest/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Bots
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Bots.initWith(OMCSettings(appId: "5b6c4e39023aae00220a1293")) { (error: Error?, userInfo: [AnyHashable : Any]?) in
+            
+        }
+
+        
+        
         return true
     }
 
--- ChatbotTest/ViewController.swift
+++ ChatbotTest/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Bots
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showBotButton(_ sender: Any) {
+        Bots.show()
+    }
+    
 }
 
